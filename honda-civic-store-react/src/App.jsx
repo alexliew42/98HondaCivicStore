a3de065f80440d38e1112ca58738b4e05db1db0c
giosroom99 <giovannitshibangu@icloud.com>
2024-01-25 16:26:05 -0500

    Home page completed

--- honda-civic-store-react/src/App.jsx
+++ honda-civic-store-react/src/App.jsx
@@ -1,15 +1,15 @@
-import NavigationBar from './NavigationBar';
-import { Routes, Route } from 'react-router-dom';
-import Product from './Product';
-import './App.css';
+import NavigationBar from "./NavigationBar";
+import { Routes, Route } from "react-router-dom";
+import Product from "./Product";
+import "./App.css";
+import Home from "./components/home";
 
 function App() {
   return (
     <div>
-      <h1>1998 Honda Civic Store</h1>
       <NavigationBar />
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/product" element={<Product />} />
         <Route path="/accessories" element={<h1>Accessories</h1>} />
         <Route path="/gallery" element={<h1>Gallery</h1>} />
---
+++ honda-civic-store-react/src/components/home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div>
+      <img
+        id="spin"
+        src="https://media-r2.carsandbids.com/cdn-cgi/image/width=1800,quality=70/c51905b0000b639a185eeb080dd879bf007f5604/photos/KPdQOoy7.L8zeYcs0Y-(edit).jpg?t=162350711773/"
+      ></img>
+      <h2>1998 Honda Civic only 26 years old!!!!</h2>
+    </div>
+  );
+};
+
+export default Home;
